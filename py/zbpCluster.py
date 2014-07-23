0dc5bf6cf446942c44e0c8358ec085177af81641
Kara Leary <learykara@gmail.com>
2014-07-22 23:07:35 -0400

    script to find k=2 and k=5 clusters for total employees

--- py/zbpCluster.py
+++ py/zbpCluster.py
@@ -3,8 +3,9 @@ import numpy as np
 import collections
 from sklearn.cluster import KMeans
 import geopandas as gp
-import copy
 import matplotlib as plt
+from pylab import *
+import copy
 
 # dictionary with zip code as key, list of total employees as value
 emp = {}
@@ -22,25 +23,19 @@ def addYear(fileName, dictionary, ind):
 			else:
 				dictionary[zc].append(float(row[ind]))
 
-addYear("../output/zbp/12totals.txt", emp, 4)
-addYear("../output/zbp/11totals.txt", emp, 4)
-addYear("../output/zbp/10totals.txt", emp, 4)
-addYear("../output/zbp/09totals.txt", emp, 4)
-addYear("../output/zbp/08totals.txt", emp, 4)
-addYear("../output/zbp/07totals.txt", emp, 4)
-addYear("../output/zbp/06totals.txt", emp, 3)
-addYear("../output/zbp/05totals.txt", emp, 3)
-addYear("../output/zbp/04totals.txt", emp, 3)
-addYear("../output/zbp/03totals.txt", emp, 3)
-addYear("../output/zbp/02totals.txt", emp, 3)
-addYear("../output/zbp/01totals.txt", emp, 3)
 addYear("../output/zbp/00totals.txt", emp, 3)
-addYear("../output/zbp/99totals.txt", emp, 3)
-addYear("../output/zbp/98totals.txt", emp, 3)
-addYear("../output/zbp/97totals.txt", emp, 3)
-addYear("../output/zbp/96totals.txt", emp, 3)
-addYear("../output/zbp/95totals.txt", emp, 3)
-addYear("../output/zbp/94totals.txt", emp, 3)
+addYear("../output/zbp/01totals.txt", emp, 3)
+addYear("../output/zbp/02totals.txt", emp, 3)
+addYear("../output/zbp/03totals.txt", emp, 3)
+addYear("../output/zbp/04totals.txt", emp, 3)
+addYear("../output/zbp/05totals.txt", emp, 3)
+addYear("../output/zbp/06totals.txt", emp, 3)
+addYear("../output/zbp/07totals.txt", emp, 4)
+addYear("../output/zbp/08totals.txt", emp, 4)
+addYear("../output/zbp/09totals.txt", emp, 4)
+addYear("../output/zbp/10totals.txt", emp, 4)
+addYear("../output/zbp/11totals.txt", emp, 4)
+addYear("../output/zbp/12totals.txt", emp, 4)
 
 # 5 zip codes with missing entries are removed and ignored
 ignore = ['10065', '10069', '11109', '11425', '10075']
@@ -65,37 +60,20 @@ ordered = collections.OrderedDict(sorted(emp.items()))
 # Create numpy array of feature vectors that is consistent with the
 # ordered list of zip codes.
 i = 0
-fv = np.zeros(shape = (len(ordered), 19))
+gfv = np.zeros(shape = (len(zcta), 13))
 for zipCode in ordered:
-	for j in range(0, 19):
-		fv[i][j] = emp[zipCode][j]
+	for j in range(0, 13):
+		gfv[i][j] = emp[zipCode][j]
 	i += 1
 
-# normalize fv entries:
-#fvNorm = np.zeros(shape = (len(ordered), 19))
-#normVars = []
-#for feature in fv:
-#	std = np.std(feature)
-#	mean = np.mean(feature)
-#	normVars.append([mean, std])
-
-#normalizedFV = fv
-
-#for i in range(0, len(fv)):
-#	for j in range(0, 19):
-#		normalizedFV[i][j] = fv[i][j] - normVars[i][0]
-#		normalizedFV[i][j] = fv[i][j] / normVars[i][1]
-
-
 # taking only 2000-2012:
-gfv = copy.deepcopy(fv[:,6:])
+#gfv = copy.deepcopy(kfv)
 gfv = (gfv.T - gfv.mean(1)).T
 gfv = (gfv.T/gfv.std(1)).T
 
 # generate n=5 clusters
 kmeans = KMeans(init='random', n_clusters=5, n_init=10)
 kmeans.fit(gfv)
-
 
 # create plots of five classes on one figure:
 fig = figure()
@@ -128,40 +106,57 @@ plot(kmeans.cluster_centers_.T[:,ind],lw=2,color='purple')
 text(-14.5, 15, 'Total Number of Employees\n k = 5', size=16)
 
 
-# create map of different classes:
-zips = gp.GeoDataFrame.from_file('../data/nyc_zipcta/nyc_zipcta.shp')
-#zips.plot()
+# create geodataframe of different classes:
+#gdf = gp.GeoDataFrame.from_file('../output/nyc-zip-code-extend.json')
+gdf = gp.GeoDataFrame.from_file('../data/nyc_zipcta/nyc_zipcta.shp')
 
-# first need to filter out zip codes that don't appear in either
-# geodataframe "zips" or in the feature vectors:
-#noncommonZips = []
-#fvzips = zips['ZCTA5CE00']
-#fvzips2 = []
-#for entry in fvzips:
-#	fvzips2.append(int(entry))
-#for entry in fvzips2:
-#	if entry not in zcta:
-#		if entry not in noncommonZips:
-#			noncommonZips.append(entry)
-#for entry in zcta:
-#	if int(entry) not in fvzips2:
-#		if entry not in noncommonZips:
-#			noncommonZips.append(entry)
+# dictionary of cluster labels for each zip code
+labels = {}
+for i in range(0, len(zcta)):
+	labels[zcta[i]] = kmeans.labels_[i]
 
-#dropThese = []
-#for i in range(0, len(zips)):
-#	zc = zips.loc[i].ZCTA5CE00
-#	if int(zc) in noncommonZips:
-#		dropThese.append(i)
+# for reference, create a list of zip codes that are not in both
+# kmeans.labels_ and the geodataframe "zips"
+noncommonZips = []
+#fvzips = gdf['ZIP']
+fvzips = gdf['ZCTA5CE00']
+fvzips2 = []
+for entry in fvzips:
+	fvzips2.append(int(entry))
+for entry in fvzips2:
+	if entry not in zcta:
+		if entry not in noncommonZips:
+			noncommonZips.append(entry)
+for entry in zcta:
+	if int(entry) not in fvzips2:
+		if entry not in noncommonZips:
+			noncommonZips.append(entry)
 
-#newgdf = zips.drop(dropThese)
+# add "clusterLabels" to existing gdf
+labelList = []
+zipList = []
+for i in range(0, len(gdf)):
+#	geozip = int(gdf.loc[i].ZIP)
+	geozip = int(gdf.loc[i].ZCTA5CE00)
+	try:
+		labelList.append(labels[geozip])
+		zipList.append(geozip)
+	except:
+		labelList.append(-1)
+		zipList.append(geozip)
+
+gdf['clusterLabel'] = labelList
+
+#figure()
+#gdf.plot(column='clusterLabel', colormap='Accent')
+
+gdf.to_file('../output/clusterPlots/shapefiles/employeesZip.shp')
 
 
+# sum(kmeans.labels_==0)
+# 0: 29
+# 1: 22
+# 2: 35
+# 3: 61
+# 4: 63
 
-#zips['cluster'] = -1
-
-#clusterLabels = {}
-#for i in range(0, len(zcta)):
-#	zc = zcta[i]
-#	label = kmeans.labels_[i]
-#	clusterLabels[]
